f3073b6575c1d7308c4790feb3d89c006f13ebb4
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-20 15:02:05 -0700

    now have the bugs fixed and works properly

--- js/scrips.js
+++ js/scrips.js
@@ -1,41 +1,41 @@
-function add(num1, num2){
+function add(num1, num2) {
   return num1 + num2;
 }
- function subtract(num1, num2){
-  return num1 - num1;
- }
-function multiply(num1, num2){
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
   return num1 * num2;
 }
-function divide(num1, num2){
-  return num1, num2;
+
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-
-function handleCalulations(event){
+// User Interface Logic
+function handleCalculation(event) {
   event.preventDefault();
   const number1 = parseInt(document.querySelector("input#input1").value);
   const number2 = parseInt(document.querySelector("input#input2").value);
-  const operator = document.querySelector("input[name = 'flavor']:checked").value;
+  const operator = document.querySelector("input[name='operator']:checked").value;
+
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+
+  document.getElementById("output").innerText = result;
 }
 
-let results;
-if (operator === 'add') {
-  results = add(num1, num2);
-} else if (operator === 'subtract'){
-  results = subtract(num1, num2);
-} else if (operator === 'multiply'){
-  results = multiply(num1, num2);
-} else if ( operator === 'divide'){
-  results = divide( num1, num2);
-
-}
-
-document.getElementById('input').innerText = results;{
-
-}
-
-window.addEventListener('load', function(){
-  const form = document.getElementById('calculator');
-  form.addEventListener('submit', calculatorFunction); 
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator")
+  form.addEventListener("submit", handleCalculation);
 });
