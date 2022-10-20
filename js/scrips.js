cbb5bd16a1ba194189aa271234ad41199fc20eb6
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-20 11:02:11 -0700

    not stop rereshing the page. trying to figure out the bug

---
+++ js/scrips.js
@@ -0,0 +1,41 @@
+function add(num1, num2){
+  return num1 + num2;
+}
+ function subtract(num1, num2){
+  return num1 - num1;
+ }
+function multiply(num1, num2){
+  return num1 * num2;
+}
+function divide(num1, num2){
+  return num1, num2;
+}
+
+
+function calculatorFunction(event){
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name = 'flavor']:checked").value;
+}
+
+let results;
+if (operator === 'add') {
+  results = add(num1, num2);
+} else if (operator === 'subtract'){
+  results = subtract(num1, num2);
+} else if (operator === 'multiply'){
+  results = multiply(num1, num2);
+} else if ( operator === 'divide'){
+  results = divide( num1, num2);
+
+}
+
+document.getElementById('input').innerText = results;{
+
+}
+
+window.addEventListener('load', function(){
+  const form = document.getElementById('calculator');
+  form.addEventListener('submit', calculatorFunction); 
+});
